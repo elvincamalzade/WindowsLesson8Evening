a58cb2d88d02560750a9154cd84f4e5a3cc6af7e
Elvin Camalzade <e.camalzade@itstep.edu.az>
2021-04-01 16:05:53 +0400

    Add project files.

---
+++ MVP/Views/MainView.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace MVP.Views
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.modelTxtb = new System.Windows.Forms.TextBox();
+            this.vendorTxtb = new System.Windows.Forms.TextBox();
+            this.colorTxtb = new System.Windows.Forms.TextBox();
+            this.yearTxtb = new System.Windows.Forms.TextBox();
+            this.transmissionTxtb = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.dataListBox = new System.Windows.Forms.ListBox();
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(59, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Model";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(59, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Vendor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(59, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Color";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(59, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Year";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(59, 279);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Transmission";
+            // 
+            // modelTxtb
+            // 
+            this.modelTxtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.modelTxtb.Location = new System.Drawing.Point(203, 38);
+            this.modelTxtb.Name = "modelTxtb";
+            this.modelTxtb.Size = new System.Drawing.Size(100, 26);
+            this.modelTxtb.TabIndex = 5;
+            // 
+            // vendorTxtb
+            // 
+            this.vendorTxtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vendorTxtb.Location = new System.Drawing.Point(203, 87);
+            this.vendorTxtb.Name = "vendorTxtb";
+            this.vendorTxtb.Size = new System.Drawing.Size(100, 26);
+            this.vendorTxtb.TabIndex = 6;
+            // 
+            // colorTxtb
+            // 
+            this.colorTxtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.colorTxtb.Location = new System.Drawing.Point(203, 142);
+            this.colorTxtb.Name = "colorTxtb";
+            this.colorTxtb.Size = new System.Drawing.Size(100, 26);
+            this.colorTxtb.TabIndex = 7;
+            // 
+            // yearTxtb
+            // 
+            this.yearTxtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.yearTxtb.Location = new System.Drawing.Point(203, 225);
+            this.yearTxtb.Name = "yearTxtb";
+            this.yearTxtb.Size = new System.Drawing.Size(100, 26);
+            this.yearTxtb.TabIndex = 8;
+            // 
+            // transmissionTxtb
+            // 
+            this.transmissionTxtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.transmissionTxtb.Location = new System.Drawing.Point(203, 273);
+            this.transmissionTxtb.Name = "transmissionTxtb";
+            this.transmissionTxtb.Size = new System.Drawing.Size(100, 26);
+            this.transmissionTxtb.TabIndex = 9;
+            // 
+            // addBtn
+            // 
+            this.addBtn.AutoSize = true;
+            this.addBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addBtn.Location = new System.Drawing.Point(203, 336);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(100, 30);
+            this.addBtn.TabIndex = 10;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // dataListBox
+            // 
+            this.dataListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dataListBox.FormattingEnabled = true;
+            this.dataListBox.ItemHeight = 20;
+            this.dataListBox.Location = new System.Drawing.Point(515, 38);
+            this.dataListBox.Name = "dataListBox";
+            this.dataListBox.Size = new System.Drawing.Size(229, 244);
+            this.dataListBox.TabIndex = 11;
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.AutoSize = true;
+            this.loadBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loadBtn.Location = new System.Drawing.Point(515, 327);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(229, 30);
+            this.loadBtn.TabIndex = 12;
+            this.loadBtn.Text = "Load";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loadBtn);
+            this.Controls.Add(this.dataListBox);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.transmissionTxtb);
+            this.Controls.Add(this.yearTxtb);
+            this.Controls.Add(this.colorTxtb);
+            this.Controls.Add(this.vendorTxtb);
+            this.Controls.Add(this.modelTxtb);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainView";
+            this.Text = "MainView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox modelTxtb;
+        private System.Windows.Forms.TextBox vendorTxtb;
+        private System.Windows.Forms.TextBox colorTxtb;
+        private System.Windows.Forms.TextBox yearTxtb;
+        private System.Windows.Forms.TextBox transmissionTxtb;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.ListBox dataListBox;
+        private System.Windows.Forms.Button loadBtn;
+    }
+}
